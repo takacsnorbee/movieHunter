7f04da5dfbb4765d3cbb00411c780797905bef1f
takacsnorbee <38784102+takacsnorbee@users.noreply.github.com>
2022-09-21 09:23:22 +0200

    material ui card

--- movie-hunter/src/components/MovieData/MovieData.jsx
+++ movie-hunter/src/components/MovieData/MovieData.jsx
@@ -1,10 +1,66 @@
 import './MovieData.css';
-import Button from '../../common/Button/Button';
+// import Button from '../../common/Button/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import StarIcon from '@mui/icons-material/Star';
 
 const MovieData = ({ movieData, handleHideData }) => {
   return (
     <aside>
-      <p>{movieData.title}</p>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography
+            className='card-date-wrapper'
+            color='text.secondary'
+            gutterBottom
+          >
+            <span>{movieData.genre}</span>
+            <span>{movieData.released}</span>
+          </Typography>
+          <Typography variant='h5' component='div'>
+            {movieData.title}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color='text.secondary'>
+            {movieData.actors}
+          </Typography>
+          <Typography sx={{ fontSize: 14 }} variant='body2'>
+            <StarIcon className='star-icon' /> {movieData.imdbRatings}
+          </Typography>
+        </CardContent>
+        <CardActions className='card-link-btns-wrapper'>
+          {movieData.imdbID && (
+            <Button
+              className='card-link-btns'
+              target='_blank'
+              variant='contained'
+              href={`https://www.imdb.com/title/${movieData.imdbID}/`}
+            >
+              IMDB
+            </Button>
+          )}
+          {movieData.wikiID && (
+            <Button
+              className='card-link-btns'
+              target='_blank'
+              variant='contained'
+              href={`https://en.wikipedia.org/?curid=${movieData.wikiID}`}
+            >
+              WIKIPEDIA
+            </Button>
+          )}
+          <Button
+            className='card-link-btns'
+            variant='contained'
+            onClick={handleHideData}
+          >
+            CLOSE
+          </Button>
+        </CardActions>
+      </Card>
+      {/* <p>{movieData.title}</p>
       <p>{movieData.actors}</p>
       <p>{movieData.details}</p>
       <p>{movieData.genre}</p>
@@ -32,7 +88,7 @@ const MovieData = ({ movieData, handleHideData }) => {
         className='close-panel-btn'
         handleClick={handleHideData}
         title='Close'
-      />
+      /> */}
     </aside>
   );
 };
